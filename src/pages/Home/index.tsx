4cfc05d62ef9624f797431c7a769117156bd833a
Arthur Pereira da Silva <arthur.pereira@academico.ifpb.edu.br>
2022-10-16 22:26:13 -0300

    feat(home/profile): add structure

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,39 @@
 import React from 'react';
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <main>
+      <h1>Anotações</h1>
+      <h2>Faça anotações de um jeito mais fácil : )</h2>
+      <section>
+        <div>
+          <div>
+            <h3>Nome da lista</h3>
+            <button>X</button>
+          </div>
+          <ul>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+          </ul>
+        </div>
+        <div>
+          <div>
+            <h3>Nome da lista</h3>
+            <button>X</button>
+          </div>
+          <ul>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+            <li>Item</li>
+          </ul>
+        </div>
+      </section>
+      <div>
+        <button>Criar nova lista</button>
+      </div>
+    </main>
+  );
 }
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
 export default function Profile() {
-  return <div>Profile</div>;
+  return (
+    <div>
+      <div>FOTO</div>
+      <input type="text" />
+      <input type="email" />
+      <button>Salvar</button>
+    </div>
+  );
 }
